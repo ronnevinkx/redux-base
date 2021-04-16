e260f177cd70f092e803f6f4fc232e89a57fa393
ronnevinkx <ronnevinkx@gmail.com>
2021-04-16 16:24:56 +0200

    Chrome Redux tab support

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,10 +1,13 @@
-import { combineReducers, createStore } from 'redux';
-import counterReducer from './ducks/counter';
+import { combineReducers, createStore } from "redux";
+import counterReducer from "./ducks/counter";
 
 const reducer = combineReducers({
-	counter: counterReducer
+  counter: counterReducer,
 });
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
